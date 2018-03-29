7ecde3d55e352526d0696d7ba5ed6e7f7d243200
MacDada <kontakt@dnowak.pl>
2018-03-29 03:22:00 +0200

    frontend: grid => table

--- assets/app.js
+++ assets/app.js
@@ -1,7 +1,7 @@
 // todo: don't know why import works and this does not: const Vue = require('vue');
 import Vue from 'vue';
 
-import Grid from './vue_components/grid.vue';
+import Table from './vue_components/table.vue';
 
 require('./app.scss');
 
@@ -11,13 +11,13 @@ new Vue({
     el: '#demo',
     data: {
         searchQuery: '',
-        gridColumns: ['name', 'power'],
-        gridData: [
+        columns: ['name', 'power'],
+        data: [
             { name: 'Chuck Norris', power: Infinity },
             { name: 'Bruce Lee', power: 9000 },
             { name: 'Jackie Chan', power: 7000 },
             { name: 'Jet Li', power: 8000 }
         ]
     },
-    components: { Grid }
+    components: { AppTable: Table }
 });
